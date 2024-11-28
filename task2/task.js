4fc1512d35d5c37418faff63f570262caf36dcce
Dantitanx <email.daniel.illes@gmail.com>
2024-11-28 13:27:35 +0100

    Task 2 done

--- task2/task.js
+++ task2/task.js
@@ -1,3 +1,15 @@
 export function findUniqueGenres(genres) {
 
+    const uniqueGenres = [];
+
+    genres.forEach(movieGenres => {
+        movieGenres.forEach(genre => {
+            if(!uniqueGenres.includes(genre)){
+                uniqueGenres.push(genre)
+            }
+        });
+    });
+
+    return uniqueGenres;
+
 }
