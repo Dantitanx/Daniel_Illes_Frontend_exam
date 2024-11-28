db250fcd1cc78f3a1d86728c20abee7e6bce6093
Dantitanx <email.daniel.illes@gmail.com>
2024-11-28 13:26:34 +0100

    Finished the first task

--- task1/task.js
+++ task1/task.js
@@ -1,3 +1,11 @@
 export function findByActorNameAndGenre(allMovies, allActors, actorName, genre) {
+const actor = allActors.find(actor => actor.name === actorName);
+if(!actor){
+    return [];
+}
+
+const matchingMovies = allMovies.filter(movie => movie.actor_ids.includes(actor.id) && movie.genres.includes(genre));
+
+return matchingMovies.map(movie => movie.title);
 
 }
